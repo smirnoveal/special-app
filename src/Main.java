a37552d84120b18f033a3f80686c0fcd377fd3d7
Evgeniy Smirnov <smirnoveal@yandex.ru>
2023-12-16 15:31:51 +0300

    sout 1+1 removed

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
         System.out.println(formatter.format(new Date()));
-        System.out.println(1+1);
+
     }
 }
