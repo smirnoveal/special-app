2bb282733f8c1bc3e868dc61562534e9fd25999b
Evgeniy Smirnov <smirnoveal@yandex.ru>
2023-12-16 16:30:51 +0300

    task module 2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println(formatter.format(new Date()));
 
         System.out.println("89998135795");
+        System.out.println("564654646546544");
 
 
     }
