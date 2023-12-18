4b542603383c895f14ddb2a495d9fe46b35b8c5d
Evgeniy Smirnov <smirnoveal@yandex.ru>
2023-12-18 23:29:58 +0300

    new task new branch

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,12 @@ public class Main {
     public static void main(String[] args) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
         System.out.println(formatter.format(new Date()));
-        System.out.println("Выберите пунктдоставки");
-
-        System.out.println("Добавим кнопку");
-
-        System.out.println("Добавим еще кнопку");
-
-        System.out.println("Изменения из сервера");
 
 
-        System.out.println("89998156456");
-        System.out.println("89998135465");
+        System.out.println("Делаем задание 3.4");
+
+
+
 
 
 
