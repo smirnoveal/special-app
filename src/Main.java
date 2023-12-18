cea54593b534367fc0ad7eb2e147a05d4d3022a0
Evgeniy Smirnov <smirnoveal@yandex.ru>
2023-12-18 23:11:00 +0300

    basket-delivery-option

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
         System.out.println(formatter.format(new Date()));
+        System.out.println("Выберите пунктдоставки");
 
 
 
