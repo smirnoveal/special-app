5e3d885d40f96d9b410f1e9277b8c77f903eaeec
smirnoveal <123317783+smirnoveal@users.noreply.github.com>
2023-12-18 23:31:52 +0300

    task branch server

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,8 @@ public class Main {
         System.out.println("89998156456");
         System.out.println("89998135465");
 
+        System.out.println("Изменения из сервера по заданию");
+
 
 
     }
